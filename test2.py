fb34947f205ed8f2ed8af72c411a4222074760d8
reutapel <reutapel88@gmail.com>
2019-01-31 16:25:18 +0200

    Delete test2.py

--- test2.py
+++
@@ -1,7 +0,0 @@
-import string
-word = 'Teamsters'
-invalidChars = set(string.punctuation)
-
-if (not word.islower() and not word.isupper() or word.isupper()) \
-     and (word not in invalidChars and not word.isdigit()):
-    print(True)
